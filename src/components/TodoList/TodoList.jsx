f549f5ac67042c2ed01edeec1306bdccb93e64e9
Miraz9907 <meraz.rahman99@gmail.com>
2024-02-16 12:04:40 +0600

    basic card design

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,9 +1,47 @@
-
+import "./TodoList.css"
 
 const TodoList = () => {
+   
     return (
         <div>
-            <h1>My Awesome ToDo List</h1>
+            <h1 className="title">My Awesome ToDo List</h1>
+            <div className="container">
+                <div className="card">
+                    <label>
+                        Task: 
+                        <input type="text" />
+                    </label>
+                    <label>
+                        Priority:
+                        <select>
+                            <option value="low">Low</option>
+                            <option value="medium">Medium</option>
+                            <option value="high">High</option>
+                        </select>
+                    </label>
+                    <label>
+                        Status:
+                        <select>
+                            <option value="incomplete">Incompleted</option>
+                            <option value="complete">Completed</option>
+                        </select>
+                    </label>
+                    <div className="add-btn">
+                    <button >Add Task</button>
+                    </div>
+                </div>
+
+                <div className="card">
+
+
+                <div>
+                    <p>Total Tasks: {}</p>
+                    <p>Completed Tasks: {}</p>
+                    <p>Incompleted Tasks: {}</p>
+                </div>
+                </div>
+
+            </div>
         </div>
     );
 };
